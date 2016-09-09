5ea7fa1c0542db9dd8d415c287c3d175607f0e04
@.@ <_@localhost.localdomain>
2016-09-09 13:38:19 +0200

    init

---
+++ backend/TranslationTR.js
@@ -0,0 +1,18 @@
+
+class TranslationDE {
+}
+TranslationDE['Welcome'] = 'Willkommen';
+TranslationDE['WelcomeBody'] = 'Willkommen bei Future4Talents...';
+TranslationDE['ResetPassword'] = 'Passwort vergessen?';
+TranslationDE['ResetPasswordMailBody'] = '....';
+TranslationDE['ApplicantInvite'] = 'Du wurdest eingeladen';
+TranslationDE['ApplicantInviteMailBody'] = '...';
+TranslationDE['BasicTestAllowed'] = '...';
+TranslationDE['BasicTestAllowedMailBody'] = '...';
+TranslationDE['BasicTestResult'] = '...';
+TranslationDE['BasicTestResultMailBody'] = '...';
+
+
+
+global.Translator['de'] = TranslationDE;
+
